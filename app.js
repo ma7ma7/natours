a0e1d03fd3d9b1500f79fe7a6e4096e1745f880e
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-02-05 20:43:44 +0100

    Basic routes handlers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,8 +10,12 @@ const tours = JSON.parse(
     encoding: 'utf8',
   })
 );
+// Use middleware
+app.use(express.json());
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
+// Handling Routs
+app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'Success',
     NumberOfTours: tours.length,
@@ -20,7 +25,84 @@ app.get('/', (req, res) => {
   });
 });
 
-const port = 3000;
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Error',
+      message: 'Coud not find tour',
+    });
+  }
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const tour = Object.assign({ id: newId }, req.body);
+  tours.push(tour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'Success',
+        data: {
+          tour: tour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Error',
+      message: 'Coud not find tour',
+    });
+  }
+
+  res.status(200).json({
+    status: 'Success',
+    data: {
+      tour: '<Tour will updated>',
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((tour) => tour.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'Error',
+      message: 'Coud not find tour',
+    });
+  }
+
+  res.status(204).json({
+    status: 'Success',
+    data: {
+      tour: '<Tour will deletd>',
+    },
+  });
+});
+
+// Server
+const port = 8000;
 app.listen(port, () => {
-  console.log('Listning');
+  console.log('Listning ...', port);
 });
