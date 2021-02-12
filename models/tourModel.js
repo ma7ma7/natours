a3d4609733d01f3c84720392c465f1fc513444f7
Amane Mahmoud <mahmoudamane5@gmail.com>
2021-02-12 16:41:46 +0100

    Validate tour data on Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,6 +4,71 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
+    unique: true,
+    trim: true,
+    maxLength: [40, 'A tour name must have less or equal then 40 characters'],
+    minLength: [10, 'A tour name must have more or equal then 10 characters'],
+  },
+  slug: String,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a maxGroupSize'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'A difficulty is either: easy, medium or difficult',
+    },
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above than 1'],
+    max: [5, 'Rating must be less than 5'],
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+  priceDiscount: {
+    type: Number,
+    validate: function (discount) {
+      // this.price here is point on the current document price value
+      return discount < this.price;
+    },
+  },
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a imageCover'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+  startsDate: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
   },
 });
 
